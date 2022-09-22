15dcd01ee3d20f851b06af5aab29c935574403a6
Mayowaogun <mayowaogun01@gmail.com>
2022-09-22 21:09:09 +0100

    task 14

---
+++ MyWorld/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! I am looking forward to becoming the best at writing a java script");
+    }
+}
